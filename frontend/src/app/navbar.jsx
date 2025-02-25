d76ee1b2f193956ed044e87bf016c216e50d8476
Akhilesh Kumar <akhileshyadav90863@gmail.com>
2025-02-25 12:02:45 +0530

    ..

--- frontend/src/app/navbar.jsx
+++ frontend/src/app/navbar.jsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 // import useAppContext from '@/context/appContext'
 const navbar = () => {
   // const { loggedIn, logout } = useAppContext();
+  
   return (
     <div>
       <header className="shadow-md bg-white font-[sans-serif] tracking-wide relative z-50">
@@ -34,7 +35,7 @@ const navbar = () => {
                   </div>
                 ) : ( */}
                  <div className="flex  md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse"> 
-            <Link href="/signup">
+            <Link href="/mainpages/signUp">
             <button
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
